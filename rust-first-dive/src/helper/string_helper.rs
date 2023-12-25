868a7f3437ad5ef95ee2eae4a789695afe3b5c28
jaffarabbas <gamaportal8@gmail.com>
2023-12-26 00:02:23 +0500

    Commited File : string_helper.rs at 26/12/2023 12:02:22 am

---
+++ rust-first-dive/src/helper/string_helper.rs
@@ -0,0 +1,11 @@
+pub mod string_helper{
+    pub fn calculate_length(s: &String) -> usize {
+        s.len()
+    }
+}
+
+pub mod string_helper_v2{
+    pub fn calculate_length2(s: &String) -> usize {
+        s.len()
+    }
+}
