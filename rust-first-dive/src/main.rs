7313e778c260709b4b631199180b2e4f400bebc7
jaffarabbas <gamaportal8@gmail.com>
2023-12-28 23:51:16 +0500

    update driver class

--- rust-first-dive/src/main.rs
+++ rust-first-dive/src/main.rs
@@ -15,14 +15,25 @@ use casting::casting;
 use conditions::condition;
 use functions::{add, sub};
 
+pub fn test(){
+    println!("Select your functions");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    let input: u32 = input.trim().parse().unwrap();
+    match input {
+        1 => owner(),
+        2 => borrow(),
+        3 => scoping(),
+        4 => dataTypes(),
+        5 => inputs(),
+        6 => casting(),
+        7 => condition(),
+        8 => println!("{}",add(1, 3)),
+        9 => println!("{}",sub(1, 3)),
+        _ => println!("Invalid input"),
+    }
+}
+
 fn main() {
-    // owner();
-    // borrow();
-    // scoping();
-    // dataTypes();
-    // inputs();
-    // casting();
-    // condition();
-    // println!("{}",add(1, 3));
-    // println!("{}",sub(1, 3));
+    test();
 }
